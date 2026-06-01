c26acf2eaecd06b1e0788b4e4abc95a9bfaa81d1
Qubes Researcher <research@qubes-check.local>
2026-06-01 06:48:14 +0200

    gnu: qubes: Format system and vm modules with guix style.
    * modules/qubes/system.scm: Run guix style.
    * modules/qubes/vm.scm: Likewise.

--- modules/qubes/system.scm
+++ modules/qubes/system.scm
@@ -12,37 +12,34 @@
   #:use-module (gnu system privilege)
   #:use-module (qubes packages)
   #:use-module (qubes services)
-  #:export (%qubes-privileged-programs
-            %qubes-system-services
-            qubes-host-name
-            qubes-operating-system))
+  #:export (%qubes-privileged-programs %qubes-system-services qubes-host-name
+                                       qubes-operating-system))
 
 (define %qubes-privileged-programs
   (cons (privileged-program
-         (program (file-append qubes-vm-core "/lib/qubes/qfile-unpacker"))
-         (setuid? #t))
-        %default-privileged-programs))
+          (program (file-append qubes-vm-core "/lib/qubes/qfile-unpacker"))
+          (setuid? #t)) %default-privileged-programs))
 
 (define %qubes-system-services
   ;; The full service stack a Qubes Guix TemplateVM runs.  It is the same for
   ;; both variants (the minimal variant is GUI-capable, it just ships fewer
   ;; applications).  Composed here as one exported list so the template's
   ;; operating-system form can reference it directly:
-  ;;   - the session/system D-Bus used by GUI and Qubes tooling;
-  ;;   - the Qubes VM GUI + headless services (qrexec, QubesDB, meminfo-writer,
-  ;;     networking, updates proxy, GUI agent, ...);
-  ;;   - the Qubes sysctl service;
-  ;;   - the trimmed set of Guix base services Qubes does not own.
-  (append (list (service dbus-root-service-type))
-          %qubes-vm-gui-services
-          (list %qubes-sysctl-service)
-          %qubes-minimal-base-services))
+  ;; - the session/system D-Bus used by GUI and Qubes tooling;
+  ;; - the Qubes VM GUI + headless services (qrexec, QubesDB, meminfo-writer,
+  ;; networking, updates proxy, GUI agent, ...);
+  ;; - the Qubes sysctl service;
+  ;; - the trimmed set of Guix base services Qubes does not own.
+  (append (list (service dbus-root-service-type)) %qubes-vm-gui-services
+          (list %qubes-sysctl-service) %qubes-minimal-base-services))
 
 (define (qubes-host-name variant)
   "Return the Qubes TemplateVM host name for VARIANT ('normal or 'minimal)."
   (case variant
-    ((minimal) "guix-minimal-qubes")
-    ((normal) "guix-qubes")
+    ((minimal)
+     "guix-minimal-qubes")
+    ((normal)
+     "guix-qubes")
     (else (error "unsupported Qubes Guix template variant" variant))))
 
 (define* (qubes-operating-system #:key (variant 'normal))
@@ -56,43 +53,37 @@
     ;; by the operating-system type and is needed for "guix system init" to copy
     ;; the full store closure, but images are built with --no-bootloader so no
     ;; boot code is written to disk.
-    (bootloader
-     (bootloader-configuration
-      (bootloader grub-bootloader)
-      (targets '("/dev/xvda"))))
+    (bootloader (bootloader-configuration
+                  (bootloader grub-bootloader)
+                  (targets '("/dev/xvda"))))
     (kernel qubes-dom0-kernel)
     (initrd-modules '())
-    (kernel-arguments
-     (append '("console=hvc0" "panic=1")
-             %default-kernel-arguments))
+    (kernel-arguments (append '("console=hvc0" "panic=1")
+                              %default-kernel-arguments))
 
-    (file-systems
-     (cons* (file-system
-              (mount-point "/")
-              (device (file-system-label "guix-root"))
-              (type "ext4"))
-            %base-file-systems))
-    (swap-devices
-     (list (swap-space
-            (target "/dev/xvdc1"))))
+    (file-systems (cons* (file-system
+                           (mount-point "/")
+                           (device (file-system-label "guix-root"))
+                           (type "ext4")) %base-file-systems))
+    (swap-devices (list (swap-space
+                          (target "/dev/xvdc1"))))
 
-    (users
-     (cons* (user-account
-              (name "user")
-              (comment "Qubes user")
-              (group "users")
-              (supplementary-groups '("wheel" "netdev" "audio" "video"
-                                      "qubes")))
-            %base-user-accounts))
-    (groups
-     (cons* (user-group (name "qubes"))
-            %base-groups))
+    (users (cons* (user-account
+                    (name "user")
+                    (comment "Qubes user")
+                    (group "users")
+                    (supplementary-groups '("wheel" "netdev" "audio" "video"
+                                            "qubes"))) %base-user-accounts))
+    (groups (cons* (user-group
+                     (name "qubes")) %base-groups))
 
     (packages (qubes-variant-packages variant))
 
-    (services %qubes-system-services)
+    (services
+     %qubes-system-services)
 
     (privileged-programs %qubes-privileged-programs)
-    (sudoers-file
-     (plain-file "sudoers"
-                 "root ALL=(ALL) ALL\n%wheel ALL=(ALL) NOPASSWD:ALL\nuser ALL=(ALL) NOPASSWD:ALL\n"))))
+    (sudoers-file (plain-file "sudoers" "root ALL=(ALL) ALL
+%wheel ALL=(ALL) NOPASSWD:ALL
+user ALL=(ALL) NOPASSWD:ALL
+"))))
--- modules/qubes/vm.scm
+++ modules/qubes/vm.scm
@@ -5,26 +5,25 @@
   #:use-module (qubes packages)
   #:use-module (qubes services)
   #:use-module (qubes system)
-  #:re-export (qubes-release-version
-               qubes-libvchan-xen
-               qubes-linux-utils-qrexec
-               qubes-vm-utils
-               qubesdb-vm
-               qubes-vm-qrexec
-               qubes-vm-core
-               qubes-vm-gui-common
-               qubes-vm-gui
-               pipewire-qubes
-               qubes-dom0-kernel
-               %qubes-vm-headless-packages
-               %qubes-vm-gui-packages
-               %qubes-normal-audio-packages
-               %qubes-normal-desktop-packages
-               %qubes-common-packages
-               qubes-variant-packages
-               %qubes-network-sysctl-settings
-               %qubes-privileged-programs
-               %qubes-system-services
-               qubes-host-name
-               qubes-operating-system
-               xterm-desktop-entry))
+  #:re-export (qubes-release-version qubes-libvchan-xen
+                                     qubes-linux-utils-qrexec
+                                     qubes-vm-utils
+                                     qubesdb-vm
+                                     qubes-vm-qrexec
+                                     qubes-vm-core
+                                     qubes-vm-gui-common
+                                     qubes-vm-gui
+                                     pipewire-qubes
+                                     qubes-dom0-kernel
+                                     %qubes-vm-headless-packages
+                                     %qubes-vm-gui-packages
+                                     %qubes-normal-audio-packages
+                                     %qubes-normal-desktop-packages
+                                     %qubes-common-packages
+                                     qubes-variant-packages
+                                     %qubes-network-sysctl-settings
+                                     %qubes-privileged-programs
+                                     %qubes-system-services
+                                     qubes-host-name
+                                     qubes-operating-system
+                                     xterm-desktop-entry))
